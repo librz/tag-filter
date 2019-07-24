b8cb14b6ba153aedab04fe22f9741f279b9cad04
Patrick Ren <50607948+Patrick-Ren@users.noreply.github.com>
2019-07-24 19:28:48 +0800

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from 'react-dom'
+import Filter from './components/Filter'
+
+ReactDOM.render(
+    <Filter />,
+    document.getElementById('root')
+);
+
