2619c58579ff1e92804c0239f3a2e0674fe8d583
Patrick Ren <50607948+Patrick-Ren@users.noreply.github.com>
2019-07-25 15:01:12 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 import React from "react";
-import ReactDOM from 'react-dom'
-import Filter from './components/Filter'
+import ReactDOM from "react-dom";
+import Filter from "./components/Filter";
+
+const Hobby = {
+  title: "爱好",
+  data: ["运动", "阅读", "电子游戏", "编程", "音乐", "电影"]
+};
+
+const AgeRange = {
+  title: "年龄",
+  data: ["12岁以下", "12~18", "18~60", "60岁以上"]
+};
 
 ReactDOM.render(
-    <Filter />,
-    document.getElementById('root')
+  <Filter initialData={[Hobby, AgeRange]} />,
+  document.getElementById("root")
 );
-
